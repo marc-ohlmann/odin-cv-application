5fada46a5931a582b13a0f572438e1feebf811d4
Marc Ohlmann <marc.ohlmann25@gmail.com>
2022-08-18 18:03:16 -0700

    implemented the CvJobTask and CvJobTaskList components, completing the CvExperience component

--- src/components/CvJobTask.js
+++ src/components/CvJobTask.js
@@ -1,6 +1,50 @@
+import { toHaveFocus } from "@testing-library/jest-dom/dist/matchers";
 import React from "react";
 import CvEditableInput from "./CvEditableInput"
 
 const jobTask = (description) => {
   return {description}
 };
+
+
+export default class CvJobTask extends CvEditableInput {
+    constructor(props)
+    {
+      super(props);
+
+      this.state = {
+        ...this.state,
+      }
+      
+      this.handleInputChanged = this.handleInputChanged.bind(this);
+      this.handleDeleteBtnClicked = this.handleDeleteBtnClicked.bind(this);
+    }
+
+    handleInputChanged()
+    {
+      let inputDescription = document.querySelector("#input-description" + this.props.listId).value;
+
+
+      this.props.onChange(this.props.listId, inputDescription);
+    }
+
+    handleDeleteBtnClicked()
+    {
+      this.props.onDelete(this.props.listId);
+    }
+  
+    render(){
+      return this.props.bCanEdit ? (
+        <div>
+          <input type="text" id={"input-description" + this.props.listId} value={this.props.description} name={this.props.listId} onChange={this.handleInputChanged}/>
+          <button onClick={this.handleDeleteBtnClicked}>Delete</button>
+        </div>
+      ) : (
+        <div>
+          {this.props.description}
+        </div>
+      );
+    }
+  }
+  
+  export {CvJobTask, jobTask};
--- src/components/CvJobTaskList.js
+++ src/components/CvJobTaskList.js
@@ -1,30 +1,65 @@
 import React from "react";
-
+import {CvJobTask, jobTask} from "./CvJobTask";
 
 export default class CvJobTaskList extends React.Component {
     constructor(props)
     {
       super(props);
+
+      this.state = {
+        tasks: []
+      }
   
-      //this.handleAddBtnClicked = this.handleAddBtnClicked.bind(this);
+      this.handleAddBtnClicked = this.handleAddBtnClicked.bind(this);
+      this.handleTaskChanged = this.handleTaskChanged.bind(this);
+      this.handleTaskDeleted = this.handleTaskDeleted.bind(this);
     }
   
-    /*handleAddBtnClicked()
+    handleAddBtnClicked()
     {
       this.setState({
-        //
+        tasks: this.state.tasks.concat(jobTask(""))
       });
-    }*/
+    }
+
+    handleTaskChanged(listId, description)
+    {
+      let new_tasks = this.state.tasks;
+
+      new_tasks[listId].description = description;
+
+      this.setState({
+        tasks: new_tasks
+      });
+
+      console.log(new_tasks);
+    }
+
+    handleTaskDeleted(listId)
+    {
+      this.setState({
+        tasks: this.state.tasks.filter((_, i) => i !== listId)
+      });
+    }
   
     render(){
+      var taskList = [];
+  
+      for(let i = 0; i < this.state.tasks.length; i++)
+      {
+        taskList.push(
+          <CvJobTask description={this.state.tasks[i].description} listId={i} onDelete={this.handleTaskDeleted} onChange={this.handleTaskChanged} bCanEdit={this.props.bCanEdit}/>
+        );
+      }
+
       return this.props.bCanEdit ? (
         <div>
-          {this.props.title}
-          <button>Add Task</button>
+          {taskList}
+          <button id="addBtn" onClick={this.handleAddBtnClicked}>Add</button>
         </div>
       ) : (
         <div>
-          {this.props.title}
+          {taskList}
         </div>
       );
     }
